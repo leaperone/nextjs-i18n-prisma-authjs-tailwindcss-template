c7ebeac92c7a6cca3ee5620378cfa654c775e1d3
cunoe <cunkycheng@gmail.com>
2025-07-29 21:01:03 +0800

    fix(i18n): set cookie expiration for switchLocaleAction

--- i18n/switch-locale.ts
+++ i18n/switch-locale.ts
@@ -1,10 +1,12 @@
 'use server';
 
-import {cookies} from 'next/headers';
-import {LANGUAGE_COOKIE} from '../i18n/settings';
+import { cookies } from 'next/headers';
+import { LANGUAGE_COOKIE } from '../i18n/settings';
 
 export async function switchLocaleAction(value: string) {
-  (await cookies()).set(LANGUAGE_COOKIE, value);
+  (await cookies()).set(LANGUAGE_COOKIE, value, {
+    expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000), // 1 year
+  });
 
   // It does not matter what we return here
   return {
